65d919d9dc53b3638093d1dabd8b35192d6e7241
topthinking <topthinking@ximalaya.com>
2019-04-15 11:40:22 +0800

    添加d.ts

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+declare module 'open-chrome-refresh' {
+  /**
+   *
+   * 示例
+   *
+   * ```js
+   *
+   *	const open = require('open-chrome-refresh');
+   *	open('https://github.com/Topthinking/open-chrome-refresh') // open
+   *	open('https://github.com/Topthinking/open-chrome-refresh') // refresh!
+   *
+   * ```
+   */
+  export default function open(url: string) {}
+}
